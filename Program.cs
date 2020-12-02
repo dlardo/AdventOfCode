2098dcbf33c3cd51f5d71bfeebc7ba05e5283d48
Doug Lardo <doug.lardo@gmail.com>
2020-12-01 23:29:54 -0800

    day1 and almost day2

--- Program.cs
+++ Program.cs
@@ -2,17 +2,115 @@
 
 namespace AdventOfCode
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Day1();
+            Day2();
         }
 
-        static void Day1()
+        // https://adventofcode.com/2020/day/2
+        private static void Day2()
+        {
+            string[] examplePasswordPolicy = { "1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc" };
+            string[] PasswordPolicy = ReadInputData("day2.txt");
+
+            int violations = 0;
+            int passes = 0;
+
+            foreach (var item in PasswordPolicy)
+            {
+                // Pull out interesting values
+                int dashPos = item.IndexOf("-");
+                int minFreq = Convert.ToInt32(item.Substring(0, dashPos));
+                // Console.WriteLine($"minFreq: [{minFreq}]");
+
+                int maxFreq = Convert.ToInt32(Between(item, "-", " "));
+                // Console.WriteLine($"maxFreq: [{maxFreq}]");
+
+                string reqLetter = Between(item, " ", ":");
+
+                // Console.WriteLine($"reqLetter: [{reqLetter}]");
+
+                string password = item.Substring(item.IndexOf(": ") + 2);
+                // Console.WriteLine($"password: [{password}]");
+
+                // Password meets policy logic
+                // The password policy indicates the lowest and highest number of times 
+                // a given letter must appear for the password to be valid
+
+                // Count number of reqLetters
+                int lettersFound = CountStringOccurrences(password, reqLetter);
+
+                Console.WriteLine(item);
+
+                // test if under min or over max
+                if (lettersFound < minFreq)
+                {
+                    // if yes, record violation   
+                    Console.WriteLine($"MinF {lettersFound} < {minFreq} Letter: {reqLetter} Password: {password}");
+                    violations++;
+                }
+
+                else if (lettersFound > maxFreq)
+                {
+                    Console.WriteLine($"MaxF {lettersFound} > {maxFreq} Letter: {reqLetter} Password: {password}");
+                    violations++;
+                }
+                else
+                {
+                    Console.WriteLine($"Pass {minFreq} <= {lettersFound} <= {maxFreq} Letter: {reqLetter} Password: {password}");
+                    passes++;
+                }
+            }
+
+            Console.WriteLine($"Number of Violations: {violations}/{PasswordPolicy.Length}");
+            Console.WriteLine($"Number of Passes: {passes}/{PasswordPolicy.Length}");
+        }
+
+        private static string[] ReadInputData(string filename)
+        {
+            string fullpath = @"C:\Users\dlardo\Nextcloud\Documents\github.com\dlardo\AdventOfCode\input data\" + filename;
+            return System.IO.File.ReadAllLines(fullpath);
+        }
+
+        private static string Between(string STR, string FirstString, string LastString)
+        {
+            string FinalString;
+            int Pos1 = STR.IndexOf(FirstString) + FirstString.Length;
+            int Pos2 = STR.IndexOf(LastString);
+            FinalString = STR.Substring(Pos1, Pos2 - Pos1);
+            return FinalString;
+        }
+
+        /// <summary>
+        /// Count occurrences of strings.
+        /// </summary>
+        private static int CountStringOccurrences(string text, string pattern)
+        {
+            // Loop through all instances of the string 'text'.
+            int count = 0;
+            int i = 0;
+            while ((i = text.IndexOf(pattern, i)) != -1)
+            {
+                i += pattern.Length;
+                count++;
+            }
+            return count;
+        }
+
+        // https://adventofcode.com/2020/day/1
+        private static void Day1()
         {
             int[] sampleExpenseReport = { 1721, 979, 366, 299, 675, 1456 };
-            int[] expenseReport = { 1973, 1755, 1601, 1852, 493, 1905, 1752, 1946, 1608, 1438, 1383, 1281, 1918, 1125, 1624, 1802, 1513, 1574, 1871, 1831, 1842, 1869, 1982, 1027, 1009, 1020, 1016, 1336, 1519, 1721, 1960, 1889, 1299, 1355, 1622, 399, 1919, 1749, 1709, 1425, 1789, 1620, 1071, 1248, 1786, 1879, 1208, 1697, 1643, 1510, 1578, 1152, 1592, 1985, 1653, 1112, 591, 1784, 1393, 1607, 1130, 1054, 1120, 1619, 1029, 1453, 1850, 1451, 1753, 1483, 1021, 1553, 1561, 1656, 1975, 1600, 1677, 1912, 1334, 1526, 1345, 1115, 2010, 1758, 1664, 1102, 1588, 1339, 1745, 1605, 1638, 1599, 1741, 1147, 1837, 1142, 1774, 1562, 1936, 1810, 1648, 1576, 1794, 1621, 1194, 1246, 1727, 1915, 1793, 1037, 1587, 1103, 1947, 1116, 1567, 1528, 1964, 1163, 1980, 1672, 1773, 1593, 1586, 169, 1613, 1712, 1854, 1632, 1760, 1182, 1812, 1811, 1660, 1728, 1067, 1835, 1501, 1335, 1647, 1853, 543, 1108, 1024, 1559, 1717, 1826, 1544, 1585, 1655, 1386, 1331, 1485, 1537, 1290, 1941, 1734, 2003, 1151, 1679, 1782, 1783, 1146, 1277, 1766, 1900, 530, 1955, 1268, 1968, 1432, 1170, 1867, 1005, 1202, 1564, 1096, 1707, 1309, 1094, 1295, 1974, 1404, 1229, 1883, 1838, 1997, 1536, 408, 1149, 1945, 1454, 1856, 1792, 1614, 1492, 1823, 1803, 1533, 1726, 1364 };
+            string[] expenseReportRaw = ReadInputData("day1.txt");
+            int[] expenseReport = new int[expenseReportRaw.Length];
+
+            // Convert the array into int[]
+            for (int i = 0; i < expenseReportRaw.Length; i++)
+            {
+                expenseReport[i] = Convert.ToInt32(expenseReportRaw[i]);
+            }
 
             foreach (var item in expenseReport)
             {
